3b6ec79d0257918beaebf0f381c70247b004829c
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2022-10-29 20:40:43 +0700

    1st-modules-alt_export_modules

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const names = require('./name');
 const greetings = require('./utils');
+const items = require('./items');
+
 
 // 1. Reusable modules
 greetings(names.john);
+console.log(items.singleItemObj);
+console.log(items.items);
